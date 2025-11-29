8190bbf05628ccb075d39ad92cdf36c874902361
Ebrahim Haji <ebrahimhaji@Ebrahims-Mac-mini.local>
2025-11-28 20:33:19 -0600

    feat: added the ability to deserial bulk string

--- redis.cpp
+++ redis.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string>
 #include <valarray>
+#include <vector>
 
 using namespace std;
 
@@ -34,10 +35,28 @@ string de_serial(string value) {
     if (value.empty()) return "";
 
     char type_resp = value[0];
-    string extract_value = value.substr(0, value.find("\r\n"));
+    string extract_value = value.substr(1, value.find("\r\n"));
 
+    // check for simple string, error, int
     if (type_resp == '+' || type_resp == '-' || type_resp == ':') {
-        return value.substr(1, value.find("\r\n"));
+        return extract_value;
+    }
+
+    // check for null bulk and array string
+    if (extract_value.substr(0,2) == "-1") {
+        return "(null)";
+    }
+
+    // check for bulk string
+    int find_n = value.find('\n');
+    int position_end = stoi(value.substr(1, value.find('\r') - 1));
+
+    if (type_resp == '$') { 
+        return value.substr(find_n + 1, position_end);
+    }
+    
+    if (type_resp == '*') {
+        
     }
 
     return "none";
@@ -47,11 +66,11 @@ string de_serial(string value) {
 }
 
 int main() {
-    // string null_bulk_string = "$-1\r\n";
-    // string de_null_bulk_string = de_serial(null_bulk_string);
+    string null_bulk_string = "$-1\r\n";
+    string de_null_bulk_string = de_serial(null_bulk_string);
 
-    // string null_array_string = "*-1\r\n";
-    // string de_null_array_string = de_serial(null_array_string);
+    string null_array_string = "*-1\r\n";
+    string de_null_array_string = de_serial(null_array_string);
 
     // string ping_command = "*1\r\n$4\r\nping\r\n";
     // string de_ping_command = de_serial(ping_command);
@@ -79,10 +98,16 @@ int main() {
     string int_string = ":123\r\n";
     string de_int_string = de_serial(int_string);
 
+    string bulk_string = "$11\r\nhello world\r\n";
+    string de_bulk_string = de_serial(bulk_string);
+
     cout << de_ok_response;
     cout << '\n' << de_error_response;
     cout << '\n' << de_simple_string;
     cout << '\n' << de_int_string;
+    cout << '\n' << de_null_bulk_string;
+    cout << '\n' << de_null_array_string;
+    cout << '\n' << de_bulk_string;
 
 
 
